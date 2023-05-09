3e9a85adacaf08260bd8efb98855676513404ec9
ofer <oferc008@gmail.com>
2023-05-09 18:51:20 +0300

    add blueprint

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 from flask import Flask
+from sport import sport_bp      
+from news import news_bp      
+
 
 app = Flask(__name__)
+app.register_blueprint(sport_bp)
+app.register_blueprint(news_bp)
 
-@app.route("/")
-def hello_world():
-    print("hello")
-    return "<p>Hello, World!</p>"
-
-@app.route("/news")
-def news():
-    return "<p>This is the NEWS</p>"
-
-@app.route("/sport")
-def sport():
-    return "<p>This is the SPORT section</p>"
+@app.route('/')
+def my_start():
+    return  "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=9000)
+    app.run(debug=True, port=3000)
