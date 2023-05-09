4d5d82b6f9f0455fa0e05a90754f42b9319ba9ea
ofer <oferc008@gmail.com>
2023-05-09 21:12:56 +0300

    add example jinja2

--- news.py
+++ news.py
@@ -3,7 +3,8 @@ news_bp = Blueprint('news', __name__,url_prefix='/news')
 
 @news_bp.route('/')
 def news():
-    return render_template("news.html")
+    new_arr = [1,2,3,4,5,6,7,8,9,10]
+    return render_template("news.html",my_array = new_arr)
     # return "<p> **** This is the news ****</p>"
 
 @news_bp.route('/global')
