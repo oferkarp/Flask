ba8cc0fccfbe443bb6f0141985abc613d177e0ed
ofer <oferc008@gmail.com>
2023-05-18 17:41:42 +0300

    add login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask , render_template,request,redirect, url_for
 from sport import sport_bp      
 from news import news_bp      
 
@@ -7,9 +7,21 @@ app = Flask(__name__)
 app.register_blueprint(sport_bp)
 app.register_blueprint(news_bp)
 
-@app.route('/')
+@app.route('/' , methods=['GET','POST'])
 def my_start():
-    return  "<p>Hello, World!</p>"
+    user_name = request.form.get("username")
+    password = request.form.get("password")
+    number_x = request.form.get("X")
+    number_y = request.form.get("Y")
+    if user_name == "ofer" and password == "1234":
+        return redirect(url_for('news.news', myname=user_name))
+    if number_x is not None and number_y is not None:
+        number_x = int(number_x)
+        number_y = int(number_y)
+        result = number_x * number_y
+        return str(result) 
+    print(f" ***** {user_name}-{password}")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
--- news.py
+++ news.py
@@ -1,10 +1,11 @@
-from flask import Blueprint , render_template
+from flask import Blueprint , render_template, request
 news_bp = Blueprint('news', __name__,url_prefix='/news')
 
 @news_bp.route('/')
 def news():
+    myname = request.args.get('myname')
     new_arr = [1,2,3,4,5,6,7,8,9,10]
-    return render_template("news.html",my_array = new_arr)
+    return render_template("news.html",my_array = new_arr,myname=myname)
     # return "<p> **** This is the news ****</p>"
 
 @news_bp.route('/global')
